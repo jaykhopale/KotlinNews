9da00454586d1d7efee19d309f8cf4e9cb413226
Jayant Khopale <jkhopale@roku.com>
2020-07-28 23:39:41 -0700

    Initial commit with projet setup.

---
+++ app/src/main/java/com/jayantkhopale/kotlinreddit/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.jayantkhopale.kotlinreddit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jayantkhopale.kotlinreddit.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
