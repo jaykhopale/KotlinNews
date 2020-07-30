27eb843771e65acf5df08e69bd0e756b9cac8c72
Jayant Khopale <jkhopale@roku.com>
2020-07-29 20:51:55 -0700

    Rename UI containers to reflect functionality.

--- app/src/main/java/com/jayantkhopale/kotlinreddit/ui/NewsActivity.kt
+++ app/src/main/java/com/jayantkhopale/kotlinreddit/ui/NewsActivity.kt
@@ -1,8 +1,7 @@
 package com.jayantkhopale.kotlinreddit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jayantkhopale.kotlinreddit.ui.main.MainFragment
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,10 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
     }
 }
--- app/src/main/java/com/jayantkhopale/kotlinreddit/ui/NewsFragment.kt
+++ app/src/main/java/com/jayantkhopale/kotlinreddit/ui/NewsFragment.kt
@@ -1,24 +1,21 @@
 package com.jayantkhopale.kotlinreddit.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.navGraphViewModels
 import com.jayantkhopale.kotlinreddit.R
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
+    private val viewModel: MainViewModel by navGraphViewModels(R.id.kotlin_news_graph) {
+        defaultViewModelProviderFactory
     }
-
-    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
