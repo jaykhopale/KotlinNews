0d50d15f1b9c9e93bd35b20bd56edf77c0f16d74
Jayant Khopale <jkhopale@roku.com>
2020-07-30 05:33:31 -0700

    Add KDocs for news result.

--- app/src/main/java/com/jayantkhopale/kotlinreddit/api/NewsResult.kt
+++ app/src/main/java/com/jayantkhopale/kotlinreddit/api/NewsResult.kt
@@ -2,6 +2,9 @@ package com.jayantkhopale.kotlinreddit.api
 
 import com.jayantkhopale.kotlinreddit.data.ArticleData
 
+/**
+ * A sealed class to encapsulate results while news articles.
+ */
 sealed class NewsResult {
     object Loading : NewsResult()
     data class Success(val articles: List<ArticleData>) : NewsResult()
