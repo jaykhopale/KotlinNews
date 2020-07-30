62a344fa5a6c9a181a6e85f4de49e384159dd3ea
Jayant Khopale <jkhopale@roku.com>
2020-07-30 05:22:30 -0700

    Add relevant documentation.

--- app/src/main/java/com/jayantkhopale/kotlinreddit/api/NewsRepository.kt
+++ app/src/main/java/com/jayantkhopale/kotlinreddit/api/NewsRepository.kt
@@ -3,6 +3,9 @@ package com.jayantkhopale.kotlinreddit.api
 import com.jayantkhopale.kotlinreddit.data.NewsResponse
 import javax.inject.Inject
 
+/**
+ * A repository to fetch Kotlin news articles.
+ */
 class NewsRepository @Inject constructor(private val newsService: NewsService) {
 
     suspend fun getKotlinNews(): NewsResponse {
--- app/src/main/java/com/jayantkhopale/kotlinreddit/ui/NewsViewModel.kt
+++ app/src/main/java/com/jayantkhopale/kotlinreddit/ui/NewsViewModel.kt
@@ -7,12 +7,20 @@ import com.jayantkhopale.kotlinreddit.api.NewsResult
 import com.jayantkhopale.kotlinreddit.data.ArticleData
 import kotlinx.coroutines.Dispatchers
 
+/**
+ * A [ViewModel] to fetch Kotlin news articles from Reddit. It uses [LiveData] to let the
+ * view know about error states and successful responses.
+ */
 class NewsViewModel @ViewModelInject constructor(private val newsRepository: NewsRepository) : ViewModel() {
 
     val newsArticle: LiveData<ArticleData>
     get() = _newsArticle
     private val _newsArticle = MutableLiveData<ArticleData>()
 
+    /**
+     * A [LiveData] object to notify the view of loading, success and error states for Kotlin
+     * news articles.
+     */
     val kotlinNews: LiveData<NewsResult> = liveData(viewModelScope.coroutineContext +
             Dispatchers.IO) {
         emit(NewsResult.Loading)
